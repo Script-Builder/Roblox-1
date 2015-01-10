6b7bae3965dc470a1c31c8b3b892dbdc0ea93e4b
CoolDoctorWho <pelanyokamara@gmail.com>
2015-01-10 11:43:51 +0000

    Update Encrypt.lua

--- Odin-Framework/Modules/Encrypt.lua
+++ Odin-Framework/Modules/Encrypt.lua
@@ -1,3 +1,22 @@
 --[[
-
+  Odin Encryption Module
+  
+  Version : 0.1
+  Build__ : Boxbone
+  Type___ : ModuleScript
+  
+  Description : 
+  This is just sha1 encryption and will be changed later!
 ]]--
+
+local Module = {};
+local Fake = {};
+
+Module.Encrypt = function(String,Context,Level)
+  local ByteArray=wait();
+end;
+
+Module.Byte = function(String) --//Unfinished
+  local Return = {};
+  for Char in String:gmatch(".") do table.insert(result, string.byte(letter)) end --//Lolno.
+end;
