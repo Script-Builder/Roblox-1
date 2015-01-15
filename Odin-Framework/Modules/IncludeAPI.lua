df9a466023836f4303beac732bcf82d4c59b60d4
CoolDoctorWho <pelanyokamara@gmail.com>
2015-01-15 20:02:42 +0000

    Create IncludeAPI.lua

---
+++ Odin-Framework/Modules/IncludeAPI.lua
@@ -0,0 +1,16 @@
+--[[
+  Include API
+  
+  Version : 0.3
+  Build__ : Village Jester
+  Type___ : ModuleScript
+  
+  Description :
+  This essentially works like the require function would, loading an asset in it's script context
+]]--
+
+local API={};
+
+function API:IncludeFile(File)
+
+end
